6a8d35b32f7b3cad77e6c46f385bc7a4eb41f064
Colton Correll <colton.c.correll@gmail.com>
2024-08-13 08:54:19 -0500

    migrate from personal repo

---
+++ api/email.js
@@ -0,0 +1,22 @@
+const sgMail = require('@sendgrid/mail');
+module.exports = (req, res) => {
+
+  const { from = 'nobody@fake.com', message = 'Empty', phone = 'test' } = req.body
+
+  sgMail.setApiKey(process.env.SENDGRIDKEY);
+  const msg = {
+    to: ['colton.correll@platypusriver.com','david.bates@platypusriver.com'],
+    from: 'contact@platypusriver.com',
+    subject: 'Platypus River Contact Form',
+    text: `${message} \r\nPhone: ${phone}, Email: ${from}`
+  };
+  if(from.search('@') === -1 || phone == "555-555-1212" || ["thousands", "rich", "millionaire", "profit", "financial", "capital", "congrat", "wallet", "dollar", "money","robot","cash", "income","job", "bot", "earn", "rank", "boost", "seo", "crypto", "$"].some(x => message.toLowerCase().includes(x))){//bs spammers
+    return;
+  }
+  sgMail.send(msg).then(() => {
+    res.status(200).send('true')
+  })
+  .catch(error => {
+    res.status(200).send(error)
+  });
+}
