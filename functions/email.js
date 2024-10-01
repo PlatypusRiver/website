9841e9d922b588adc899d1a6bd6964b7ea6dc984
Colton Correll <colton.c.correll@gmail.com>
2024-10-01 15:25:38 -0500

    trying to fix email

--- functions/email.js
+++ functions/email.js
@@ -1,21 +1,25 @@
-export function onRequest(req) {
-  const sgMail = require('sendgrid-mail');
+export async function onRequestPost(req) {
   const { from = 'nobody@fake.com', message = 'Empty', phone = 'test' } = req.params
-
-  sgMail.setApiKey(process.env.SENDGRIDKEY);
-  const msg = {
-    to: ['colton.correll@platypusriver.com','david.bates@platypusriver.com'],
-    from: 'contact@platypusriver.com',
-    subject: 'Platypus River Contact Form',
-    text: `${message} \r\nPhone: ${phone}, Email: ${from}`
-  };
   if(from.search('@') === -1 || phone == "555-555-1212" || ["thousands", "rich", "millionaire", "profit", "financial", "capital", "congrat", "wallet", "dollar", "money","robot","cash", "income","job", "bot", "earn", "rank", "boost", "seo", "crypto", "$"].some(x => message.toLowerCase().includes(x))){//bs spammers
     return;
   }
-  sgMail.send(msg).then(() => {
-    res.status(200).send('true')
-  })
-  .catch(error => {
-    res.status(200).send(error)
+  await fetch("https://api.sendgrid.com/v3/mail/send", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${context.env.SENDGRID_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [{ email: 'colton.correll@platypusriver.com' }]
+        },
+      ],
+      from: { email: from },
+      subject: "Platypus River Contact Form","content": [{"type": "text/plain", "value": `${message} \r\nPhone: ${phone}, Email: ${from}`}]
+    }),
+  });
+  return new Response(null, {
+    status: 200,
   });
 }
